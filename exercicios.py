55830f6c378da0b7e64eaaff60ba8217d8718826
Vinicius Aponte <apontevinicius@gmail.com>
2024-11-20 21:03:37 -0300

    adicionando exercicio com o tipo int

---
+++ exercicios.py
@@ -0,0 +1,51 @@
+# %%
+'''
+1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+'''
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número número: '))
+
+r = f'O resultado da soma dos dois número é: {n1 + n2}'
+
+print(r)
+# %%
+'''
+2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+'''
+RESTO = 5
+n1 = int(input('Digite um número: '))
+
+r = f'O resto da divisão de {n1} por {RESTO} é de {n1 % RESTO}'
+
+print(r)
+# %%
+'''
+3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+'''
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número número: '))
+
+r = f'O resultado da multiplicação dos dois número é: {n1 + n2}'
+
+print(r)
+# %%
+'''
+4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+'''
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número número: '))
+
+r = f'O resultado da divisão inteira dos dois número é: {n1 // n2}'
+
+print(r)
+# %%
+'''
+5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+'''
+n1 = int(input('Digite um número: '))
+QUADRADO = 2
+
+r = f'O resultado do quadrado do número digitado é de: {n1 ** QUADRADO}'
+
+print(r)
+# %%
